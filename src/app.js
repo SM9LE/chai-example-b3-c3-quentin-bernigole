cf728ccd50ac7bfe0496063902ba75d5f963577f
SM9LE <quentin.bernigole@hotmail.com>
2022-11-30 10:29:47 +0100

    Adding a Triangle function & Triangle tests

--- src/app.js
+++ src/app.js
@@ -16,6 +16,50 @@ class Cube {
     }
 }
 
+class Triangle {
+    constructor(sideA, sideB, sideC, hauteur) {
+        this.sideA = sideA;
+        this.sideB = sideB;
+        this.sideC = sideC;
+        this.hauteur = hauteur;
+    }
+
+    isRightAngledTriangle () {
+        let result1 = Math.pow(this.sideC, 2);
+        let result2 = Math.pow(this.sideA, 2);
+        let result3 = Math.pow(this.sideB, 2);
+
+        if (result1 === result2 + result3)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    getSurfaceRightAngledTriangle () {
+        return (this.sideA * this.sideB) / 2;
+    }
+
+    isIsocelesTriangle () {
+        if (this.sideA === this.sideB || this.sideB === this.sideA && this.sideB === this.sideC && this.sideA === this.sideB)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    getSurfaceAnyTriangle () {
+        return (this.sideA * this.hauteur)/2
+    }
+}
+
 module.exports = {
-    Cube:Cube
+    Cube:Cube,
+    Triangle:Triangle
 }
--- test/app.test.js
+++ test/app.test.js
@@ -1,23 +1,61 @@
 const Cube = require('../src/app').Cube;
+const Triangle = require('../src/app').Triangle;
 const expect = require('chai').expect;
+const assert = require('chai').assert;
+
+// Tests Cube
 
 describe('Testing the Cube Functions', function() {
-    it('1. The side length of the Cube', function(done) {
+    it('1. The side length of the Cube', function (done) {
         let c1 = new Cube(2);
         expect(c1.getSideLength()).to.equal(2);
         done();
     });
-    
-    it('2. The surface area of the Cube', function(done) {
+
+    it('2. The surface area of the Cube', function (done) {
         let c2 = new Cube(5);
         expect(c2.getSurfaceArea()).to.equal(150);
         done();
     });
-    
-    it('3. The volume of the Cube', function(done) {
+
+    it('3. The volume of the Cube', function (done) {
         let c3 = new Cube(7);
         expect(c3.getVolume()).to.equal(343);
         done();
     });
-    
-});
+},
+
+// Test Triangles
+
+describe('Testing the Triangle Functions', function() {
+    it('1. Is it a right-angled Triangle', function(done) {
+        let t1 = new Triangle(4, 3, 2);
+        let t2 = new Triangle(4, 3, 5);
+        assert.equal(t1.isRightAngledTriangle(), false);
+        assert.equal(t2.isRightAngledTriangle(), true);
+        done();
+    });
+
+    it('2. The surface area of right-angled Triangle', function(done) {
+        let t3 = new Triangle(4, 3, 5);
+        expect(t3.getSurfaceRightAngledTriangle()).to.equal(6);
+        done();
+    });
+
+    it('3. Is it an isoceles Triangle', function(done) {
+        let t4 = new Triangle(5, 5, 3);
+        let t5 = new Triangle (6, 6, 6);
+        let t6 = new Triangle(3, 4, 4);
+        assert.equal(t4.isIsocelesTriangle(), true);
+        assert.equal(t5.isIsocelesTriangle(), true);
+        assert.equal(t6.isIsocelesTriangle(), false);
+        done();
+    });
+
+    it('4. The surface area of any Triangle', function(done) {
+        let t7 = new Triangle(5, 2, 2, 3);
+        expect(t7.getSurfaceAnyTriangle()).to.equal(7.5);
+        done();
+    });
+
+}));
